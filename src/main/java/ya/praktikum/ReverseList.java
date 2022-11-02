8580ea7cc1d9088c4dc15b73b2f74c76f1a39570
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-02 16:53:43 +0100

    add javadoc

--- src/main/java/ya/praktikum/ReverseList.java
+++ src/main/java/ya/praktikum/ReverseList.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class ReverseList {
 
+    /**
+     * [1, 2, 3, 4] => [4, 3, 2, 1]
+     */
     public void reverseInPlace(List<Integer> list) {
         if (list == null) return;
 
