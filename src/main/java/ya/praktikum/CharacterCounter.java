efacb8976f02adfdd97e4c44060f342608d7258b
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-03 17:52:20 +0100

    move javadoc

--- src/main/java/ya/praktikum/CharacterCounter.java
+++ src/main/java/ya/praktikum/CharacterCounter.java
@@ -2,20 +2,21 @@ package ya.praktikum;
 
 import java.util.Arrays;
 
+/**
+ * java -cp target/classes ya.praktikum.CharacterCounter Hello World
+ * ======
+ * H: 1
+ * e: 1
+ * l: 3
+ * o: 2
+ * W: 1
+ * r: 1
+ * d: 1
+ * ======
+ * homework: ignore case
+ * homework: sort by count
+ */
 public class CharacterCounter {
-    /**
-     * java -cp target/classes ya.praktikum.CharacterCounter Hello World
-     * H: 1
-     * e: 1
-     * l: 3
-     * o: 2
-     * W: 1
-     * r: 1
-     * d: 1
-     * ======
-     * homework: ignore case
-     * homework: sort by count
-     */
     public static void main(String[] args) {
         System.out.println(Arrays.toString(args));
         System.out.println(String.join(" ", args));
