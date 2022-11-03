bfb24c20602c60c55b84d5c44c6206f51bf86811
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-03 12:13:37 +0100

    streamline list tests

--- src/test/java/ya/praktikum/StringListTest.java
+++ src/test/java/ya/praktikum/StringListTest.java
@@ -3,6 +3,7 @@ package ya.praktikum;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -11,29 +12,21 @@ public class StringListTest {
 
     private final List<String> list = oneTwoThreeList();
 
-    @Test public void createListAndPrint() {
-        System.out.println(list);
+    @Test public void emptyList() {
+        var list = new ArrayList<Integer>();
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+    }
+
+    @Test public void nonEmptyList() {
+        var list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+
+        assertFalse(list.isEmpty());
         assertEquals(4, list.size());
-    }
-
-    @Test public void getByIndex() {
-        String value = list.get(1);
-        System.out.println(value);
-        assertEquals("two", value);
-    }
-
-    @Test public void replaceSecondItem() {
-        list.set(1, "newbie");
-
-        assertEquals(4, list.size());
-        assertEquals("newbie", list.get(1));
-    }
-
-    @Test public void removeItemByIndex() {
-        list.remove(1);
-
-        assertEquals(3, list.size());
-        assertEquals(List.of("one", "three", "four"), list);
     }
 
     @Test public void clearList() {
@@ -43,19 +36,58 @@ public class StringListTest {
         assertTrue(list.isEmpty());
     }
 
+    @Test public void getByIndex() {
+        String value = list.get(1);
+        assertEquals("two", value);
+    }
+
+    @Test public void iterateByIndex() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    @Test public void iterateByValue() {
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    @Test public void printList() {
+        System.out.println(list);
+    }
+
+    @Test public void replaceSecondItem() {
+        list.set(1, "newbie");
+
+        assertEquals(4, list.size());
+        assertEquals("newbie", list.get(1));
+    }
+
+    @Test public void compareLists() {
+        list.set(1, "newbie");
+
+        assertEquals(List.of("one", "newbie", "three", "four"), list);
+    }
+
+    @Test public void removeByIndex() {
+        list.remove(1);
+
+        assertEquals(Arrays.asList("one", "three", "four"), list);
+    }
+
+    @Test public void removeByValue() {
+        list.remove("two");
+
+        assertEquals(List.of("one", "three", "four"), list);
+    }
+
     @Test public void listContains() {
         assertTrue(list.contains("three"));
         assertFalse(list.contains("fifty"));
     }
 
-    @Test public void removeItemByValue() {
-        list.remove("two");
-
-        assertEquals(3, list.size());
-        assertEquals(List.of("one", "three", "four"), list);
-    }
-
-    @Test public void quizRemoveDuplicateItemByValue() {
+    @Test public void quizRemoveByValueWithDuplicates() {
         list.add("two");
 
         list.removeIf("two"::equals);
