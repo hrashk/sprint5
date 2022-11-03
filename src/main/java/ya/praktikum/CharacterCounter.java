492a6aac017f7dc0c2b22171822b10befdece9ce
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-03 17:21:13 +0100

    add character counter

---
+++ src/main/java/ya/praktikum/CharacterCounter.java
@@ -0,0 +1,20 @@
+package ya.praktikum;
+
+import java.util.Arrays;
+
+public class CharacterCounter {
+    /**
+     * java -cp target/classes ya.praktikum.CharacterCounter Hello World
+     * H: 1
+     * e: 1
+     * l: 3
+     * o: 2
+     * W: 1
+     * r: 1
+     * d: 1
+     */
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+        System.out.println(String.join(" ", args));
+    }
+}
