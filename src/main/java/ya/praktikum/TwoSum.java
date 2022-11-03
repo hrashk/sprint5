a6f90001d2fe60e277061d8e8401f8783becfb17
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-03 18:11:50 +0100

    add two sum puzzle

---
+++ src/main/java/ya/praktikum/TwoSum.java
@@ -0,0 +1,16 @@
+package ya.praktikum;
+
+import java.util.List;
+
+/**
+ * find a, b in [1, 3, 6, 7, 9] so that a + b = 10
+ * ===
+ * 3, 7
+ * ===
+ * homework: find indexes, i.e. [1, 3]
+ */
+public class TwoSum {
+    public List<Integer> findPair(List<Integer> numbers, int sum) {
+        return null;
+    }
+}
---
+++ src/test/java/ya/praktikum/TwoSumTest.java
@@ -0,0 +1,21 @@
+package ya.praktikum;
+
+import org.junit.Test;
+
+public class TwoSumTest {
+
+    @Test public void nullList() {}
+    
+    @Test public void emptyList() {}
+    
+    @Test public void singleElementList() {}
+
+    /**
+     * [3,3] @ 6
+     * [3,2,4] @ 6
+     * [2,7,11,15] @ 9
+     * [1, 3, 6, 7, 9] @ 10
+     */
+    @Test public void otherLists() {}
+
+}
