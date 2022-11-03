950d7fb00f24668c4f139f991a093c9d83916c4a
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-03 17:47:43 +0100

    add char tests

---
+++ src/test/java/ya/praktikum/CharacterCounterTest.java
@@ -0,0 +1,13 @@
+package ya.praktikum;
+
+import org.junit.Test;
+
+public class CharacterCounterTest {
+    @Test public void nullString() {}
+
+    @Test public void emptyString() {}
+
+    @Test public void singleCharString() {}
+
+    @Test public void multiCharString() {}
+}
