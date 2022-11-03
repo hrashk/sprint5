f31a7b4d3fed88167d5b5b18b71a2450419aa631
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-03 17:49:38 +0100

    add person set tests

---
+++ src/test/java/ya/praktikum/PersonSetTest.java
@@ -0,0 +1,18 @@
+package ya.praktikum;
+
+import org.junit.Test;
+
+public class PersonSetTest {
+
+    @Test public void nonEmptySet() {}
+
+    @Test public void printSet() {}
+
+    @Test public void contains() {}
+
+    @Test public void addDuplicate() {}
+
+    @Test public void remove() {}
+
+    @Test public void compareSets() {}
+}
