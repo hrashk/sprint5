375cb9c2af5c7893b7bdf1c615894d8735749063
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-03 17:41:48 +0100

    add string set tests

---
+++ src/test/java/ya/praktikum/StringSetTest.java
@@ -0,0 +1,21 @@
+package ya.praktikum;
+
+import org.junit.Test;
+
+public class StringSetTest {
+
+    @Test public void emptySet() {
+    }
+
+    @Test public void nonEmptySet() {}
+
+    @Test public void printSet() {}
+
+    @Test public void setLiteral() {}
+
+    @Test public void containsItem() {}
+
+    @Test public void remove() {}
+
+    @Test public void compareSets() {}
+}
