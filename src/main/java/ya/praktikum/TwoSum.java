6285a28046d4c8420b0360eeb8feb38394e907f6
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-07 20:47:43 +0100

    add two sum solution

--- src/main/java/ya/praktikum/TwoSum.java
+++ src/main/java/ya/praktikum/TwoSum.java
@@ -1,6 +1,8 @@
 package ya.praktikum;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * find a, b in [1, 3, 6, 7, 9] so that a + b = 10
@@ -11,6 +13,20 @@ import java.util.List;
  */
 public class TwoSum {
     public List<Integer> findPair(List<Integer> numbers, int sum) {
-        return null;
+        if (numbers == null || numbers.size() < 2)
+            return List.of();
+
+        Set<Integer> seen = new HashSet<>();
+        seen.add(numbers.get(0));
+
+        for (int i = 1; i < numbers.size(); i++) {
+            int currentValue = numbers.get(i);
+            if (seen.contains(sum - currentValue)) {
+                return List.of(sum - currentValue, currentValue);
+            }
+            seen.add(currentValue);
+        }
+
+        return List.of();
     }
 }
--- src/test/java/ya/praktikum/set/TwoSumPuzzleTest.java
+++ src/test/java/ya/praktikum/set/TwoSumPuzzleTest.java
@@ -1,14 +1,30 @@
 package ya.praktikum.set;
 
 import org.junit.Test;
+import ya.praktikum.TwoSum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TwoSumPuzzleTest {
 
-    @Test public void nullList() {}
+    @Test public void nullList() {
+        var s = new TwoSum();
+        assertTrue(s.findPair(null, 10).isEmpty());
+    }
     
-    @Test public void emptyList() {}
+    @Test public void emptyList() {
+        var s = new TwoSum();
+        assertTrue(s.findPair(List.of(), 10).isEmpty());
+    }
     
-    @Test public void singleElementList() {}
+    @Test public void singleElementList() {
+        var s = new TwoSum();
+        assertTrue(s.findPair(List.of(3), 10).isEmpty());
+    }
 
     /**
      * [3,3] @ 6
@@ -16,6 +32,34 @@ public class TwoSumPuzzleTest {
      * [2,7,11,15] @ 9
      * [1, 3, 6, 7, 9] @ 10
      */
-    @Test public void otherLists() {}
+    @Test public void twoElements() {
+        var s = new TwoSum();
+        assertEquals(List.of(3, 3), s.findPair(List.of(3, 3), 6));
+    }
+
+    @Test public void twoElementsGlitch() {
+        var s = new TwoSum();
+        assertEquals(List.of(2, 4), s.findPair(List.of(2, 3, 4), 6));
+    }
+
+    @Test public void threeElements() {
+        var s = new TwoSum();
+        assertEquals(List.of(2, 4), s.findPair(List.of(3, 2, 4), 6));
+    }
+
+
+    @Test public void hugeSample() {
+        var s = new TwoSum();
+        int size = 200_000;
+        ArrayList<Integer> huge = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            huge.add(-1);
+        }
+        huge.set(0, 2);
+        huge.set(size - 1, 4);
+        assertEquals(List.of(2, 4), s.findPair(huge, 6));
+    }
+
+
 
 }
