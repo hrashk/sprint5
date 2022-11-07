9b174b651c4020ec153fc7198033d0c603f03d89
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-07 20:47:53 +0100

    add character counter solution

--- src/main/java/ya/praktikum/CharacterCounter.java
+++ src/main/java/ya/praktikum/CharacterCounter.java
@@ -1,6 +1,9 @@
 package ya.praktikum;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * java -cp target/classes ya.praktikum.CharacterCounter Hello World
@@ -18,7 +21,38 @@ import java.util.Arrays;
  */
 public class CharacterCounter {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
-        System.out.println(String.join(" ", args));
+        var line = String.join("", args);
+
+        List<String> freqs = new CharacterCounter().count(line);
+        for (String freq : freqs) {
+            System.out.println(freq);
+        }
     }
+
+    public List<String> count(String line) {
+        if (line == null)
+            return List.of();
+
+        Map<Character, Integer> freqs = computeFrequencies(line);
+        return convertToList(freqs);
+    }
+
+    private static Map<Character, Integer> computeFrequencies(String line) {
+        var freqs = new LinkedHashMap<Character, Integer>();
+
+        for (char c : line.toCharArray()) {
+            freqs.compute(c, (cur, f) -> f == null ? 1 : f + 1);
+        }
+        return freqs;
+    }
+
+    private static List<String> convertToList(Map<Character, Integer> freqs) {
+        var res = new ArrayList<String>();
+        for (Map.Entry<Character, Integer> entry : freqs.entrySet()) {
+            res.add(String.format("%s: %d", entry.getKey(), entry.getValue()));
+        }
+        return res;
+    }
+
+
 }
--- src/test/java/ya/praktikum/map/CounterPuzzleTest.java
+++ src/test/java/ya/praktikum/map/CounterPuzzleTest.java
@@ -1,13 +1,31 @@
 package ya.praktikum.map;
 
 import org.junit.Test;
+import ya.praktikum.CharacterCounter;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CounterPuzzleTest {
-    @Test public void nullString() {}
+    @Test public void nullString() {
+        CharacterCounter counter = new CharacterCounter();
+        assertTrue(counter.count(null).isEmpty());
+    }
 
-    @Test public void emptyString() {}
+    @Test public void emptyString() {
+        CharacterCounter counter = new CharacterCounter();
+        assertTrue(counter.count("").isEmpty());
+    }
 
-    @Test public void singleCharString() {}
+    @Test public void singleCharString() {
+        CharacterCounter counter = new CharacterCounter();
+        assertEquals(List.of("H: 1"), counter.count("H"));
+    }
 
-    @Test public void multiCharString() {}
+    @Test public void multiCharString() {
+        CharacterCounter counter = new CharacterCounter();
+        assertEquals(List.of("H: 1", "e: 1", "l: 2", "o: 1"), counter.count("Hello"));
+    }
 }
