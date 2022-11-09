4dfeca5b604c9c1526c5d0a004d865237b89b154
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-09 09:49:42 +0100

    update string list tests

--- src/test/java/ya/praktikum/list/StringListTest.java
+++ src/test/java/ya/praktikum/list/StringListTest.java
@@ -4,81 +4,90 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class StringListTest {
 
-    private final List<String> list = oneTwoThreeList();
-
     @Test public void emptyList() {
-        var list = new ArrayList<Integer>();
-        assertTrue(list.isEmpty());
-        assertEquals(0, list.size());
+        assertTrue(new ArrayList<Integer>().isEmpty());
+
+        assertTrue(List.of().isEmpty());
+
+        assertTrue(Arrays.asList().isEmpty());
+
+        assertTrue(Collections.emptyList().isEmpty());
     }
 
-    @Test public void nonEmptyList() {
-        var list = new ArrayList<String>();
+    private static List<String> sample() {
+        List<String> list = new ArrayList<>(); // заменить на LinkedList и запустить все тесты
         list.add("one");
         list.add("two");
         list.add("three");
         list.add("four");
 
-        assertFalse(list.isEmpty());
-        assertEquals(4, list.size());
+        return list;
     }
 
-    @Test public void printList() {
+    @Test public void nonEmptyList() {
+        var list = sample();
         System.out.println(list);
-    }
+        assertEquals(4, list.size());
 
-    @Test public void listLiteral() {
-        var list = List.of("one", "two");
+        list = List.of("one", "two");
+        System.out.println(list);
         assertEquals(2, list.size());
-    }
 
-    @Test public void listFromStringArray() {
-        var list = Arrays.asList("one", "two");
+        list = Arrays.asList("one", "two");
+        System.out.println(list);
         assertEquals(2, list.size());
 
         String[] arr = {"one", "two"};
-        assertEquals(2, Arrays.asList(arr).size());
+        list = Arrays.asList(arr);
+        System.out.println(list);
+        assertEquals(2, list.size());
     }
 
     @Test public void listFromPrimitiveArray() {
-        var list = Arrays.asList(1, 2);
-        assertEquals(2, list.size());
+        int[] arr = {1, 2};
+        var list = Arrays.asList(arr);
 
-        Integer[] arr = {1, 2};
-        assertEquals(2, Arrays.asList(arr).size());
+        System.out.println(list);
+
+        assertEquals(1, list.size()); // fatality
     }
 
     @Test public void clearList() {
+        var list = sample();
+
         list.clear();
 
-        assertEquals(0, list.size());
         assertTrue(list.isEmpty());
     }
 
     @Test public void getByIndex() {
-        String value = list.get(1);
-        assertEquals("two", value);
+        assertEquals("two", sample().get(1));
     }
 
     @Test public void iterateByIndex() {
+        var list = sample();
+
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
     }
 
     @Test public void iterateByValue() {
-        for (String s : list) {
+        for (String s : sample()) {
             System.out.println(s);
         }
     }
 
     @Test public void replaceSecondItem() {
+        var list = sample();
+
         list.set(1, "newbie");
 
         assertEquals(4, list.size());
@@ -86,43 +95,58 @@ public class StringListTest {
     }
 
     @Test public void compareLists() {
+        var list = sample();
+
         list.set(1, "newbie");
 
         assertEquals(List.of("one", "newbie", "three", "four"), list);
     }
 
     @Test public void removeByIndex() {
+        var list = sample();
+
         list.remove(1);
 
         assertEquals(Arrays.asList("one", "three", "four"), list);
     }
 
+    @Test public void listContains() {
+        assertTrue(sample().contains("three"));
+
+        assertFalse(sample().contains("fifty"));
+    }
+
     @Test public void removeByValue() {
+        var list = sample();
+        list.add("two");
+
         list.remove("two");
 
+        assertEquals(List.of("one", "three", "four", "two"), list);
+    }
+
+    @Test public void removeAllByValue() {
+        var list = sample();
+        list.add("two");
+
+        list.removeIf(s -> s.equals("two"));  // lambda
+
         assertEquals(List.of("one", "three", "four"), list);
     }
 
-    @Test public void listContains() {
-        assertTrue(list.contains("three"));
-        assertFalse(list.contains("fifty"));
+    @Test public void listSort() {
+        var list = sample();
+
+        list.sort(String::compareTo);  // Comparator
+
+        assertEquals(List.of("four", "one", "three", "two"), list);
     }
 
-    @Test public void quizRemoveByValueWithDuplicates() {
-        list.add("two");
+    @Test public void collectionSort() {
+        var list = sample();
 
-        list.removeIf("two"::equals);
+        Collections.sort(list); // String implements Comparable
 
-        assertEquals(3, list.size());
-        assertEquals(List.of("one", "three", "four"), list);
-    }
-
-    private static List<String> oneTwoThreeList() {
-        List<String> list = new ArrayList<>();
-        list.add("one");
-        list.add("two");
-        list.add("three");
-        list.add("four");
-        return list;
+        assertEquals(List.of("four", "one", "three", "two"), list);
     }
 }
