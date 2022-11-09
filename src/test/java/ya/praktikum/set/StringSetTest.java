31c9e40fa8d896388f1858ddbbca9c844f39cf31
Vaagn Khachatryan <vaagn.khachatryan@gmail.com>
2022-11-09 12:01:54 +0100

    update string set examples

--- src/test/java/ya/praktikum/set/StringSetTest.java
+++ src/test/java/ya/praktikum/set/StringSetTest.java
@@ -2,45 +2,22 @@ package ya.praktikum.set;
 
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Supplier;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class StringSetTest {
 
     @Test public void emptySet() {
-    }
+        assert new HashSet<String>().isEmpty();
 
-    @Test public void nonEmptySet() {}
+        assert Set.of().isEmpty();
 
-    @Test public void printSet() {}
-
-    @Test public void setLiteral() {}
-
-    @Test public void containsItem() {}
-
-    @Test public void remove() {
-        var set = sample();
-        set.remove("three");
-        assertEquals(6, set.size());
-    }
-
-    @Test public void compareSets() {
-        Set<String> s = sample(LinkedHashSet::new);
-        s.remove("three");
-        assertSetEquals(List.of("one", "two", "four", "five", "six", "seven"), s);
+        assert Collections.emptySet().isEmpty();
     }
 
     private static Set<String> sample() {
-        return sample(HashSet::new);
-    }
-
-    private static Set<String> sample(Supplier<Set<String>> builder) {
-        var s = builder.get();
+        var s = new HashSet<String>();
         s.add("one");
         s.add("two");
         s.add("three");
@@ -48,16 +25,60 @@ public class StringSetTest {
         s.add("five");
         s.add("six");
         s.add("seven");
+
         return s;
+    }
+
+    private static Set<String> linkedSample() {
+        return new LinkedHashSet<>(List.of("one", "two", "three", "four", "five", "six", "seven"));
+    }
+
+    private static Set<String> treeSample() {
+        return new TreeSet<>(List.of("one", "two", "three", "four", "five", "six", "seven"));
+    }
+
+    @Test public void nonEmptySet() {
+        var set = sample();
+        System.out.println(set);
+        assertEquals(7, set.size());
+
+        System.out.println(linkedSample());
+
+        System.out.println(treeSample());
+
+        System.out.println(Set.of("one", "two", "three", "four", "five", "six", "seven"));
+    }
+
+    @Test public void containsItem() {
+        assert sample().contains("three");
+    }
+
+    @Test public void interviewPrintUniqueValuesInList() {
+        var list = List.of("one", "two", "two", "three");
+
+        var set = new HashSet<>(list);
+        System.out.println(set);
+
+        assertEquals(Set.of("one", "two", "three"), set);
+    }
+
+    @Test public void remove() {
+        var set = sample();
+
+        set.remove("three");
+
+        assertEquals(6, set.size());
+    }
+
+    @Test public void compareVertical() {
+        Set<String> s = linkedSample();
+
+        s.remove("throw");
+
+        assertSetEquals(List.of("one", "two", "four", "five", "six", "seven"), s);
     }
 
     private static void assertSetEquals(Iterable<String> expected, Iterable<String> actual) {
         assertEquals(String.join("\n", expected), String.join("\n", actual));
     }
-
-    @Test public void orderedSet() {}
-
-    @Test public void sortedSet() {}
-
-    @Test public void printUniqueValuesInList() {}
 }
